4f7b24ff0eb36aa018df245f72441a47787e277b
Ishara Dissanayake <thisismaduishara@gmail.com>
2023-07-24 18:49:53 +0530

    fix: modify key verification to use key type enum instead of assistant type enum

--- utils/llm.py
+++ utils/llm.py
@@ -3,17 +3,17 @@ import streamlit as st
 from huggingface_hub import InferenceClient
 from streamlit.logger import get_logger
 
-from utils.constants import AssistantType, OPENAI_CHAT_COMPLETION_MODEL
+from utils.constants import OPENAI_CHAT_COMPLETION_MODEL, APIKeyType, TEST_PROMPT
 
 logger = get_logger(__name__)
 
 
 @st.cache_data(show_spinner=False)
-def validate_api_token(assistant_type: AssistantType, api_key: str) -> tuple[bool, str]:
-    if not assistant_type:
+def validate_api_token(api_key_type: APIKeyType, api_key: str) -> tuple[bool, str]:
+    if not api_key_type:
         return (
             False,
-            "Assistant type is not mentioned",
+            "API key type is not mentioned",
         )
 
     if not api_key:
@@ -23,19 +23,17 @@ def validate_api_token(assistant_type: AssistantType, api_key: str) -> tuple[boo
         )
 
     try:
-        if assistant_type == AssistantType.OPENAI.value:
-            logger.info(
-                f"OpenAI token validation attempt for\n{assistant_type}\n{api_key}\n"
-            )
+        if api_key_type == APIKeyType.OPENAI:
             openai.ChatCompletion.create(
                 model=OPENAI_CHAT_COMPLETION_MODEL,
-                messages=[{"role": "user", "content": "test"}],
+                messages=[{"role": "user", "content": TEST_PROMPT}],
                 api_key=api_key,
+                max_tokens=1,
             )
             logger.info("OpenAI token validated")
         else:
             client = InferenceClient(token=api_key)
-            client.text_generation(prompt="hi", max_new_tokens=1)
+            client.text_generation(prompt=TEST_PROMPT, max_new_tokens=1)
             logger.info("HuggingFace token validated")
 
     except Exception as e:
