cd9c6a1ed5fecfe8bfde5bc105b87dacce6783ce
Ishara Dissanayake <thisismaduishara@gmail.com>
2023-07-24 18:52:29 +0530

    feat: modify script to retrieve embedding type and verify the embedding key accordingly

--- build_knowledgebase.py
+++ build_knowledgebase.py
@@ -13,6 +13,9 @@ from utils.constants import (
     OPENAI_KNOWLEDGEBASE_KEY,
     HF_KNOWLEDGEBASE_KEY,
     ENV_FILE,
+    EMBEDDING_TYPE_KEY,
+    EmbeddingType,
+    APIKeyType,
 )
 from utils.llm import validate_api_token
 
@@ -25,32 +28,72 @@ load_dotenv(dotenv_path=os.path.join(os.getcwd(), ENV_FILE))
 if __name__ == "__main__":
     # initialize the knowledgebase
     logger.info("⚡ Initializing the URLs...")
-    urls = [
-        "https://www.shoutout.ai/company",
-    ]
 
     # determine assistant type
     assistant_type = os.getenv(ASSISTANT_TYPE_KEY, AssistantType.HUGGINGFACE.value)
+    embedding_type = os.getenv(EMBEDDING_TYPE_KEY, EmbeddingType.HUGGINGFACE.value)
+
     if assistant_type == AssistantType.OPENAI.value:
         assistant_type = AssistantType.OPENAI
-        api_key = os.getenv(OPENAI_API_TOKEN_KEY, None)
         knowledgebase_name = os.environ.get(OPENAI_KNOWLEDGEBASE_KEY, None)
+
+        if embedding_type == EmbeddingType.OPENAI.value:
+            embedding_type = EmbeddingType.OPENAI
+            embedding_api_key = os.getenv(OPENAI_API_TOKEN_KEY, None)
+            embedding_api_key_type = APIKeyType.OPENAI
+        else:
+            embedding_type = EmbeddingType.HUGGINGFACE
+            embedding_api_key = os.getenv(HUGGINGFACEHUB_API_TOKEN_KEY, None)
+            embedding_api_key_type = APIKeyType.HUGGINGFACE
+
     else:
         assistant_type = AssistantType.HUGGINGFACE
-        api_key = os.getenv(HUGGINGFACEHUB_API_TOKEN_KEY, None)
         knowledgebase_name = os.environ.get(HF_KNOWLEDGEBASE_KEY, None)
+        embedding_type = EmbeddingType.HUGGINGFACE
+        embedding_api_key = os.getenv(HUGGINGFACEHUB_API_TOKEN_KEY, None)
+        embedding_api_key_type = APIKeyType.HUGGINGFACE
 
-    logger.info("🗝️ Validating the API token...")
-    valid, err = validate_api_token(assistant_type=assistant_type, api_key=api_key)
-    if not valid:
-        logger.error(err)
+    if embedding_type == EmbeddingType.OPENAI:
+        urls = [
+            "https://www.shoutout.ai/pricing",
+            "https://www.shoutout.ai/company",
+            "https://www.shoutout.ai/omnichannel-inbox",
+        ]
+
+    else:
+        urls = [
+            "https://www.shoutout.ai/",
+            "https://www.shoutout.ai/pricing",
+            "https://www.shoutout.ai/company",
+            "https://www.shoutout.ai/conversational-banking",
+            "https://www.shoutout.ai/conversational-ai-for-travel-and-hospitality",
+            "https://www.shoutout.ai/conversational-ai-for-healthcare",
+            "https://www.shoutout.ai/conversational-ai-in-education",
+            "https://www.shoutout.ai/omnichannel-customer-service",
+            "https://www.shoutout.ai/website-conversion-rate-optimization-services",
+            "https://www.shoutout.ai/conversational-ai",
+            "https://www.shoutout.ai/omnichannel-inbox",
+            "https://www.shoutout.ai/knowledge-base-help-center",
+            "https://www.shoutout.ai/whatsapp-chatbot",
+            "https://www.shoutout.ai/facebook-messenger",
+            "https://www.shoutout.ai/website-chatbots",
+            "https://www.shoutout.ai/telegram-chatbots",
+            "https://www.shoutout.ai/shared-inbox",
+        ]
+
+    logger.info("🗝️ Validating the embedding API token...")
+    embedding_valid, embedding_err = validate_api_token(
+        api_key_type=embedding_api_key_type, api_key=embedding_api_key
+    )
+    if not embedding_valid:
+        logger.error(embedding_err)
         sys.exit(1)
 
-    logger.info("Building the knowledgebase")
     create_knowledgebase(
         urls=urls,
         assistant_type=assistant_type,
-        api_key=api_key,
+        embedding_type=embedding_type,
+        embedding_api_key=embedding_api_key,
         knowledgebase_name=knowledgebase_name,
     )
 
